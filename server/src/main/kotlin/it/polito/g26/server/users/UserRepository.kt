aec9f05ea97ee968880363831cd67a061560c6e4
RiccardoStud <s292520@studenti.polito.it>
2023-05-12 17:27:19 +0200

    feat: Delete user

--- server/src/main/kotlin/it/polito/g26/server/users/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package it.polito.g26.server.users
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository: JpaRepository<User, String> {
-}
