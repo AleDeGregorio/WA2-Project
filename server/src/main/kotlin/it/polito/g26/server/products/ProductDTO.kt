0d983394bb4cb0890a49360d9ee2224df4859810
cicciodepa <m.depascale1@studenti.poliba.it>
2023-05-12 16:36:57 +0200

    Lab3: updated.

--- server/src/main/kotlin/it/polito/g26/server/products/ProductDTO.kt
+++ server/src/main/kotlin/it/polito/g26/server/products/ProductDTO.kt
@@ -1,11 +1,16 @@
 package it.polito.g26.server.products
 
-data class ProductDTO (
-    val ean: String,
+import it.polito.g26.server.ticketing.tickets.TicketDTO
+
+data class ProductDTO(
+    val ean: Long,
     val name:String,
-    val brand:String
+    val brand:String,
+    val category: String,
+    val price: Double,
+    val tickets: MutableSet<TicketDTO>
     )
 
 fun Product.toDTO(): ProductDTO{
-    return ProductDTO(ean, name, brand)
+    return ProductDTO(ean, name, brand, category, price, tickets = mutableSetOf())
 }
--- server/src/test/kotlin/it/polito/g26/server/DbT1ApplicationTests.kt
+++ server/src/test/kotlin/it/polito/g26/server/DbT1ApplicationTests.kt
@@ -14,13 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
