ab743d4a8afb9189c64fd58a1a28d754c0ea3079
cicciodepa <m.depascale1@studenti.poliba.it>
2023-06-04 13:29:20 +0200

    Lab5: Started implementing signup functionality.

--- server/src/main/kotlin/it/polito/g26/server/security/signup/SignupController.kt
+++ server/src/main/kotlin/it/polito/g26/server/security/signup/SignupController.kt
@@ -1,14 +1,8 @@
 package it.polito.g26.server.security.signup
 
-import it.polito.g26.server.EmptyPostBodyException
-import it.polito.g26.server.profiles.customer.Customer
 import it.polito.g26.server.profiles.customer.CustomerDTO
 import it.polito.g26.server.profiles.customer.toEntity
-import it.polito.g26.server.security.login.LoginRequest
-import it.polito.g26.server.security.login.LoginResponse
-import it.polito.g26.server.security.login.LoginServiceImpl
 import it.polito.g26.server.security.utils.Response
-import org.keycloak.representations.AccessToken
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -20,25 +14,19 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/auth")
 class SignupController (
     private val signupServiceImpl: SignupServiceImpl
-){
+) {
 
     @PostMapping("/signup/customer")
-    fun customerSignup(@RequestBody customerDTO: CustomerDTO?, @RequestParam token: String): ResponseEntity<Response> {
+    fun customerSignup(@RequestBody customerDTO: CustomerDTO?): ResponseEntity<Response> {
         if (customerDTO != null) {
-            return signupServiceImpl.customerSignup(customerDTO.toEntity(), token)
+            return signupServiceImpl.customerSignup(customerDTO.toEntity())
         } else {
-            throw EmptyPostBodyException("Empty body for signup")
+            throw Exception("not body")
         }
-
-
     }
-    @PostMapping("/")
-    fun authenticateAdminApi(): ResponseEntity<AccessToken> {
-       //modo per trovare client secret
-        val clientSecret = "client-secret"
-        val accessToken = signupServiceImpl.authenticateAdminApi(clientSecret)
-            ?: throw RuntimeException("Authentication failed")
 
-        return ResponseEntity.ok(accessToken)
+    @PostMapping("/signuptoken")
+    fun signupToken() {
+
     }
 }
--- server/src/main/kotlin/it/polito/g26/server/security/signup/SignupServiceImpl.kt
+++ server/src/main/kotlin/it/polito/g26/server/security/signup/SignupServiceImpl.kt
@@ -1,67 +1,89 @@
 package it.polito.g26.server.security.signup
 
 import it.polito.g26.server.profiles.customer.Customer
+import it.polito.g26.server.security.login.LoginResponse
 import it.polito.g26.server.security.utils.Response
-import org.keycloak.representations.AccessToken
-import org.springframework.beans.factory.annotation.Value
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.KeycloakBuilder
+import org.keycloak.admin.client.resource.RealmResource
+import org.keycloak.representations.idm.UserRepresentation
+import org.keycloak.representations.idm.CredentialRepresentation
+import org.keycloak.representations.idm.RoleRepresentation
 import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.http.*
 import org.springframework.stereotype.Service
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 import org.springframework.web.client.RestTemplate
 
 @Service
-class SignupServiceImpl(
-    @Value("\${spring.security.oauth2.client.registration.keycloak.client-id}")
-    private val clientId: String,
-    @Value("\${spring.security.oauth2.client.registration.keycloak.authorization-grant-type}")
-    private val grantType: String
-): SignupService {
+class SignupServiceImpl {
+    val restTemplate = RestTemplate()
 
-    val restTemplate = RestTemplate();
-    val tokenUrl = "http://localhost:8080/realms/SpringBoot-Keycloak/protocol/openid-connect/token";
-    override fun authenticateAdminApi(clientSecret: String): AccessToken? {
+    private val userUrl = "http://localhost:8080/admin/realms/SpringBoot-Keycloak/users"
+    fun customerSignup(customer: Customer): ResponseEntity<Response> {
+        val keycloak: Keycloak = KeycloakBuilder.builder() //
+            .serverUrl("http://localhost:8080") //
+            .realm("SpringBoot-Keycloak") //
+            .clientId("admin-cli") //
+            .username("admin") //
+            .password("password") //
+            .build();
+
+        val realmResource = keycloak.realm("SpringBoot-Keycloak")
+        val client = realmResource.clients().findByClientId("springboot-keycloak-client")[0]
+
+        val userUrl = "http://localhost:8080/admin/realms/SpringBoot-Keycloak/users"
+        val tokenUrl = "http://localhost:8080/realms/SpringBoot-Keycloak/protocol/openid-connect/token"
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+        val map: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
+        map["client_id"] = "admin-cli"
+        map["grant_type"] = "password"
+        map["username"] = "admin"
+        map["password"] = "password"
 
-        val requestBody = "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret"
-        val requestEntity = HttpEntity(requestBody, headers)
-        val responseEntity: ResponseEntity<AccessToken> = restTemplate.exchange(
-            tokenUrl,
-            HttpMethod.POST,
-            requestEntity,
-            AccessToken::class.java
-        )
+        val httpEntity = HttpEntity<MultiValueMap<String, String>>(map, headers)
 
-        if (responseEntity.statusCode.is2xxSuccessful) {
-            return responseEntity.body
+        val response: ResponseEntity<LoginResponse> = restTemplate.postForEntity(tokenUrl, httpEntity, LoginResponse::class.java)
+        var token: String = ""
+        if (response.statusCode.is2xxSuccessful) {
+            token = response.body?.access_token!!
+            println(token)
         } else {
-            throw RuntimeException("Failed to authenticate with Keycloak Admin API. Status code: ${responseEntity.statusCodeValue}")
+            throw Exception("Invalid Request")
         }
-    }
 
-    override fun customerSignup(customer: Customer, token: String) : ResponseEntity<Response> {
-
-        val userUrl = "http://localhost:8080/auth/admin/realms/Springboot-Keycloak/users"
-        val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         headers.setBearerAuth(token)
-        val userObj = JSONObject()
-        val credentialObj = JSONObject()
-        credentialObj.put("type", "password")
-        credentialObj.put("temporary",false)
-        credentialObj.put("password", customer.password)
-        userObj.put("email", customer.email)
-        userObj.put("emailVerified", false)
-        userObj.put("enabled", false)
-        userObj.put("firstName",customer.name)
-        userObj.put("lastName",customer.surname)
-        userObj.put("realmRoles", arrayOf<String>("customer"))
-        userObj.put("username",customer.id)
-        userObj.put("credentials", arrayOf(credentialObj))
+        val customerRole = realmResource.roles().get("app_customer").toRepresentation()
+        val customerClientRole = realmResource.clients().get(client.id).roles().get("customer").toRepresentation()
+        val passwordCred = CredentialRepresentation().apply {
+            isTemporary = false
+            type = CredentialRepresentation.PASSWORD
+            value = customer.password
+        }
+        val customerRep = UserRepresentation().apply {
+            isEnabled = true
+            username = "customer-${customer.name}"
+            firstName = customer.name
+            lastName = customer.surname
+            email = customer.email
+            credentials = listOf(passwordCred)
+            realmRoles = listOf(customerRole.toString())
+        }
 
-        val httpEntity = HttpEntity<String>(userObj.toString(), headers)
-        val response : ResponseEntity<Response> = restTemplate.postForEntity(userUrl, httpEntity, Response::class.java)
+        println(headers)
+        println(customerRep.toString())
 
-        return ResponseEntity<Response>(response.body, response.statusCode)
+
+
+
+        val httpEntity2 = HttpEntity<UserRepresentation>(customerRep, headers)
+        println(httpEntity2)
+        val response2: ResponseEntity<Response> = restTemplate.exchange(userUrl, HttpMethod.POST, httpEntity2, Response::class.java)
+        println(response2)
+        return ResponseEntity<Response>(response2.body, response2.statusCode)
     }
 }
+
