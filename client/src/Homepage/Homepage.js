d2f239d3c4043b76d0f8fe2b3e5d9917f12d295f
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-07-18 16:05:09 +0200

    fixed login information, personalized customer interface

--- client/src/Homepage/Homepage.js
+++ client/src/Homepage/Homepage.js
@@ -7,10 +7,11 @@ function Homepage() {
     const navigate = useNavigate()
 
     const user = useContext(LoginContext)
+    const loggedUser = Object.keys(user).length > 0
 
     return (
         <Container fluid>
-            <h1 style={{textAlign: 'center', padding:'20px'}}>Welcome</h1>
+            <h1 style={{textAlign: 'center', padding:'20px'}}>Welcome {loggedUser ? user.name : false}</h1>
             <h2>Select your service</h2>
             <div className="d-grid gap-2" style={{padding: '30px'}}>
                 <Button variant="success" size="lg"
@@ -19,10 +20,15 @@ function Homepage() {
                 >
                     Products
                 </Button>
-                {Object.keys(user).length > 0 ? false :
+                {!loggedUser ?
                     <Button variant="danger" size="lg" onClick={() => navigate('/login')}>
                     Login
-                    </Button>
+                    </Button> : false
+                }
+                {loggedUser && user.role === "customer" ?
+                    <Button variant="primary" size="lg" onClick={() => navigate('/customerTickets')}>
+                        Tickets
+                    </Button> : false
                 }
             </div>
         </Container>
