92ff1f8c57371c9407c4cccdbfdc66b5a298244b
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-10-04 16:35:03 +0200

    fixing get tickets problem

--- server/src/main/kotlin/it/polito/g26/server/profiles/expert/ExpertDTO.kt
+++ server/src/main/kotlin/it/polito/g26/server/profiles/expert/ExpertDTO.kt
@@ -8,17 +8,17 @@ data class ExpertDTO(
     val username: String,
     val firstName: String,
     val lastName: String,
-    val password: String,
+    val password: String?,
     val email: String,
     val fields: String
 )
 
 fun Expert.toDTO() : ExpertDTO {
-    return ExpertDTO(id,  username, firstName, lastName, email, password, fields)
+    return ExpertDTO(id,  username, firstName, lastName, password, email, fields)
 }
 
 fun ExpertDTO.toEntity(): Expert {
     return Expert(id = id, username = username,
-        firstName = firstName, lastName = lastName, email =  email, password = password, fields =  fields)
+        firstName = firstName, lastName = lastName, email =  email,  password = password ?: "password", fields =  fields)
 
 }
